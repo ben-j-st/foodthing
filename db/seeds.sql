6bbae10b0715c15256d46a11451d41a3addaf36d
Ben Stephens <ben_j_stephens@hotmail.com>
2020-08-09 20:23:43 +1000

    populated table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUE("Kings Way, The", false);
+INSERT INTO burgers(burger_name, devoured) VALUE("YO My With Cheese", false);
+INSERT INTO burgers(burger_name, devoured) VALUE("Wagyu Wunder", false);
